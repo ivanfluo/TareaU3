be6ab41bc32deebdd7656165dece76fe3b60c77f
Ivań Porras <60825162+ivanfluo@users.noreply.github.com>
2020-02-09 13:18:05 -0600

    Update tarea.js

---
+++ tarea.js
@@ -0,0 +1,14 @@
+var mes=10;
+if (mes==12 || mes== 1 || mes== 2) {
+    alert("ES INVIERNO");
+}else if (mes== 3 || mes==4 || mes==5){
+    alert("ES PRIMAVERA")
+}else if(mes== 6 || mes==7 || mes==8){
+    alert("ES VERANO")
+}else if(mes== 9 || mes==10 || mes==11){
+    alert("ES OTOÑO")
+}else{
+    alert("ERROR EN ANALISIS");
+}
+
+alert("Salvador Iván Hernández Porras - Carné 20002426");
